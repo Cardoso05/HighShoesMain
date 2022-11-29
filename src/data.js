f77668fb439c826e020389cf84c080fc4792038b
Matheus Cardoso da Silva Antonio <75143307+Cardoso05@users.noreply.github.com>
2022-11-29 19:39:18 -0300

    change 1

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import slider1 from "./img/slide-lebron.png"
+import slider2 from "./img/slide-lebron-2.png"
+import slider3 from "./img/slide-lebron-3.png"
+import categorie from "./img/nike.png"
+import product from "./img/produto.png"
+
+export const sliderItems = [ 
+    {
+        id: 1,
+        img: slider1,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: slider2,
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: slider3,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+]
+export const categories = [
+    {
+        id: 1,
+        img: categorie,
+        title: "SHOES AND MORE",
+    },
+    {
+        id: 2,
+        img: categorie,
+        title: "HIGH PROMO",
+    },
+    {
+        id: 3,
+        img: categorie,
+        title: "SHIRT/CLOTHES",
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: product,
+    },
+    {
+        id: 2,
+        img: product,
+    },
+    {
+        id: 3,
+        img: product,
+    },
+    {
+        id: 4,
+        img: product,
+    },
+    {
+        id: 5,
+        img: product,
+    },
+    {
+        id: 6,
+        img: product,
+    },
+    {
+        id: 7,
+        img: product,
+    },
+    {
+        id: 8,
+        img: product,
+    }
+]
